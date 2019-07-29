adc52d9a73a32db85917a4a099017907058f825d
Shenandoah Stubbs <shenandoahstubbs@mail.weber.edu>
2019-07-29 10:43:30 -0600

    Added a shuffle method from Sedgewick to shuffle the deck before the game is dealt. Working on the dealGame method to deal the 8 cards to the table, and 22 cards to each player.

---
+++ src/main/java/GameLogic.java
@@ -0,0 +1,69 @@
+import java.util.Random;
+/*
+    GameLogic is the class used to generate all the logic and game methods
+    that create the rules for playing California Speed
+    ShenandoahStubbs
+ */
+public class GameLogic {
+    Card[] deck = new Card[52];
+    //creteDeck creates the deck of 52 playing cards used for the game
+    public void createDeck (Card[] deck)
+    {
+        int i = 0;
+        for(Suit s: Suit.values())
+        {
+            for(Rank r: Rank.values())
+            {
+                deck[i] = new Card(r, s);
+                i++;
+            }
+        }
+    }
+
+    /*
+         Sedgewick's exchange method used with the shuffle
+     */
+    public static void exch(Card[] a, int i, int j)
+    {
+        Card swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+    /*
+        Sedgewick shuffle code for an array. This will shuffle the deck so that the order
+        is always different
+     */
+    public static void shuffle(Card[] a)
+    {
+      int n = a.length;
+      for(int i = 0; i< n; i++)
+      {
+          int r = i + (int)(Math.random() * (n-i));
+          exch(a, i, r);
+      }
+    }
+    /*
+        dealGame will deal the 8 cards on the table and each player 22 cards
+     */
+
+    public void dealGame()
+    {
+        createDeck(deck);
+        shuffle(deck);
+
+        
+    }
+    /*
+        checkMove checks to see if the players move is valid
+        The players move is valid if the card they played upon has a matching
+        Rank on the board at the time of the play
+     */
+    public boolean checkMove()
+    {
+        //TODO
+        return true;
+    }
+
+
+}
+
