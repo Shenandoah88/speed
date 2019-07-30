ae2244a1108bcc78ae150b5ff2bf51eafaf6a332
austinbeauregard <austinbeauregard@mail.weber.edu>
2019-07-29 21:00:55 -0600

    touch

--- js/script.js
+++ js/script.js
@@ -5,6 +5,32 @@ webSocket.onopen = function () {
 };
 
 var dragged;
+//regular expression for testing dropzone
+var patt = /dropzone/;
+
+document.addEventListener("touchstart", function(event){
+    // store a ref. on the dragged elem
+    dragged = event.target;
+// make it half transparent
+    event.target.style.opacity = .9;
+});
+
+document.addEventListener("touchmove", function(event){
+    // store a ref. on the dragged elem
+    dragged = event.target;
+// make it half transparent
+    event.target.style.opacity = .9;
+});
+
+document.addEventListener("touchend", function(event) {
+// prevent default action (open as link for some elements)
+    event.preventDefault();
+// move dragged elem to the selected drop target
+    if (patt.test(event.target.className)) {
+        event.target.style.background = "";
+        event.target.className = 'dropzone10H';
+    }
+}, false);
 
 /* events fired on the draggable target */
 document.addEventListener("drag", function(event) {
@@ -28,25 +54,6 @@ document.addEventListener("dragover", function(event) {
 // prevent default to allow drop
     event.preventDefault();
 }, false);
-
-document.addEventListener("dragenter", function(event) {
-// highlight potential drop target when the draggable element enters it
-    if (event.target.className == "dropzone") {
-
-    }
-
-}, false);
-
-document.addEventListener("dragleave", function(event) {
-// reset background of potential drop target when the draggable element leaves it
-    if (event.target.className == "dropzone") {
-
-    }
-
-}, false);
-
-//regular expression for testing dropzone
-var patt = /dropzone/;
 
 document.addEventListener("drop", function(event) {
 // prevent default action (open as link for some elements)
