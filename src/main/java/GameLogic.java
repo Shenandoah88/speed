82786ac092b629c56b4b2c6e523df5f423d95e7a
Shenandoah Stubbs <shenandoahstubbs@mail.weber.edu>
2019-07-31 00:28:37 -0600

    Java is working and can do the following successfully: - create a deck - shuffle the deck - deal the game    - deal the 8 cards for the table    - deal 22 cards to each player    - creates 8 discard ArrayLists to be used - restart the game when there are no matches    - each player picks up the 4 stack of cards in front of them    - each stack is emptied    - the game is then re-dealt from each player's hand - checks to see if a move is valid - checks to see if there are matches on the table to begin the game.

--- src/main/java/GameLogic.java
+++ src/main/java/GameLogic.java
@@ -1,82 +1,219 @@
-import java.util.Random;
-/*
-    GameLogic is the class used to generate all the logic and game methods
-    that create the rules for playing California Speed
-    ShenandoahStubbs
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ * GameLogic contains all the logic used to play the game California Speed
+ * @author shenandoahstubbs
+ *
  */
 public class GameLogic {
-    Card[] deck = new Card[51];
-    Card[] playDeck = new Card[7];
-    Card[] playerOne = new Card[21];
-    Card[] playerTwo = new Card[21];
 
-    //creteDeck creates the deck of 52 playing cards used for the game
-    public void createDeck (Card[] deck)
-    {
-        int i = 0;
-        for(Suit s: Suit.values())
-        {
-            for(Rank r: Rank.values())
-            {
-                deck[i] = new Card(r, s);
-                i++;
+    static List<Card> deck = new ArrayList<Card>();
+    static List<Card> playDeck = new ArrayList<Card>();
+    static List<Card> playerOne = new ArrayList<Card>();
+    static List<Card> playerTwo = new ArrayList<Card>();
+    static List<Card> discardPlayerOne = new ArrayList<Card>();
+    static List<Card> discardPlayerTwo = new ArrayList<Card>();
+    static List<Card> table0 = new ArrayList<Card>();
+    static List<Card> table1 = new ArrayList<Card>();
+    static List<Card> table2 = new ArrayList<Card>();
+    static List<Card> table3 = new ArrayList<Card>();
+    static List<Card> table4 = new ArrayList<Card>();
+    static List<Card> table5 = new ArrayList<Card>();
+    static List<Card> table6 = new ArrayList<Card>();
+    static List<Card> table7 = new ArrayList<Card>();
+    static Card[] deals = new Card[51];
+
+
+    /*
+     * Creates the 52 card deck for playing
+     */
+    public static List<Card> createDeck() {
+        for (Suit s : Suit.values()) {
+            for (Rank r : Rank.values()) {
+                Card c = new Card(r, s);
+                deck.add(c);
             }
         }
+        return deck;
     }
 
-    /*
-         Sedgewick's exchange method used with the shuffle
+    /**
+     * dealGame first shuffles the deck of cards Creates lays out the 8 cards on the
+     * table for playing Deals 22 cards to each player for playing
+     * adds each card from the table array into an array of itself. These will be used
+     * for the discarding and to check matching
+     *
      */
-    public static void exch(Card[] a, int i, int j)
-    {
-        Card swap = a[i];
-        a[i] = a[j];
-        a[j] = swap;
-    }
-    /*
-        Sedgewick shuffle code for an array. This will shuffle the deck so that the order
-        is always different
-     */
-    public static void shuffle(Card[] a)
-    {
-      int n = a.length;
-      for(int i = 0; i< n; i++)
-      {
-          int r = i + (int)(Math.random() * (n-i));
-          exch(a, i, r);
-      }
-    }
-    /*
-        dealGame will deal the 8 cards on the table and each player 22 cards
-     */
+    public static void dealGame() {
+        createDeck();
+        Collections.shuffle(deck);
 
-    public void dealGame()
-    {
-        createDeck(deck);
-        shuffle(deck);
-    //TODO need to deal 8 cards to the table and 22 cards to each player. Create an array for each.
-        for(int i = 0; i < 7; i ++)
+        deck.toArray(deals);
+
+        // creates 8 playing cards
+        for (int i = 0; i < 8; i++) {
+            playDeck.add(deck.get(i));
+        }
+        //create playing hand for player 1
+        for (int j = 8; j < 30; j++) {
+            playerOne.add(deck.get(j));
+        }
+        //creates playing hand for player 2
+        for(int k = 30; k < 52; k ++)
         {
-            for(int j = 0; j < 23; j++)
+            playerTwo.add(deck.get(k));
+        }
+
+        //Adds each card from the table to a single array to be used
+        //for discarding
+        table0.add(playDeck.get(0));
+        table1.add(playDeck.get(1));
+        table2.add(playDeck.get(2));
+        table3.add(playDeck.get(3));
+        table4.add(playDeck.get(4));
+        table5.add(playDeck.get(5));
+        table6.add(playDeck.get(6));
+        table7.add(playDeck.get(7));
+
+
+
+    }
+
+    /**
+     * checkTable checks to see if there are any matching cards on the table, if
+     * there are it returns true and the game can begin. As soon as it is no longer
+     * true the game is paused and restartGame is called.
+     */
+
+    public static boolean checkTable() {
+        boolean isValid = false;
+
+        for (Card match : playDeck) {
+            Rank check = match.getR();
+            for (Card checkMatch : playDeck) {
+                Rank matchCheck = checkMatch.getR();
+                if (matchCheck == check) {
+                    return true;
+                }
+            }
+        }
+        isValid = false;
+        restartGame();
+        return isValid;
+
+    }
+
+    /**
+     * checkMove checks to see if a player move is valid and allowed meaning that
+     * the card was played on a card on the table that had another card on the table
+     * that matched in rank
+     */
+
+    public static boolean checkMove() {
+        boolean valid = checkTable();
+
+        if (checkTable() == true)
+        {
+            valid = true;
+
+        }
+        else
+        {
+            valid = false;
+            restartGame();
+        }
+
+        return valid;
+    }
+
+    /**
+     * This method picks up the 4 piles in front of each player
+     * and add them to their hand. This allows the game to continue
+     * when there are no matching cards on the table
+     */
+    public static void pickUpDiscarded()
+    {
+        //pick up for player one
+        for(Card zero : table0)
+        {
+            for(Card one : table1)
             {
-                playDeck[i] = deck[i];
-                playerOne[j] = deck[j + 1];
-                playerTwo[j] = deck[j + 2];
+                for(Card two : table2)
+                {
+                    for(Card three: table3)
+                    {
+                        playerOne.add(zero);
+                        playerOne.add(one);
+                        playerOne.add(two);
+                        playerOne.add(three);
+                    }
+                }
             }
         }
 
-    }
-    /*
-        checkMove checks to see if the players move is valid
-        The players move is valid if the card they played upon has a matching
-        Rank on the board at the time of the play
-     */
-    public boolean checkMove()
-    {
-        //TODO need to check to see which cards are active on the table and if the move is allowed
-        return true;
+        //pick up for player 2
+        for(Card four : table4)
+        {
+            for(Card five : table5)
+            {
+                for(Card six : table6)
+                {
+                    for(Card seven : table7)
+                    {
+                        playerTwo.add(four);
+                        playerTwo.add(five);
+                        playerTwo.add(six);
+                        playerTwo.add(seven);
+                    }
+                }
+            }
+        }
+
+        table0.removeAll(table0);
+        table1.removeAll(table1);
+        table2.removeAll(table2);
+        table3.removeAll(table3);
+        table4.removeAll(table4);
+        table5.removeAll(table5);
+        table6.removeAll(table6);
+        table7.removeAll(table7);
     }
 
+    public static void redealExisting()
+    {
+        playDeck.add(playerOne.get(0));
+        playDeck.add(playerOne.get(1));
+        playDeck.add(playerOne.get(2));
+        playDeck.add(playerOne.get(3));
+        playDeck.add(playerTwo.get(0));
+        playDeck.add(playerTwo.get(1));
+        playDeck.add(playerTwo.get(2));
+        playDeck.add(playerTwo.get(3));
+
+        table0.add(playDeck.get(0));
+        table1.add(playDeck.get(1));
+        table2.add(playDeck.get(2));
+        table3.add(playDeck.get(3));
+        table4.add(playDeck.get(4));
+        table5.add(playDeck.get(5));
+        table6.add(playDeck.get(6));
+        table7.add(playDeck.get(7));
+    }
+    /**
+     * restartGame is called when there are no matches available on the table to
+     * play each player picks up the 4 stack of cards in front of them, then each
+     * player deals a new 4 cards each for the table
+     */
+
+    public static void restartGame()
+    {
+        pickUpDiscarded();
+        redealExisting();
+
+    }
 
 }
-
--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -3,7 +3,7 @@ package model;
 public class Card {
 
     public enum VALUES {ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
-    public enum SUITS {CLUBS, DIAMONDS, HEARTS, SPADES}
+    public enum SUITS {CLUBS, DIAMONDS, HEARTS, SPADES};
 
     private int value;
     public int getValue() {
