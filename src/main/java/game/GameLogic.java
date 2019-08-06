4246a3cf5fbea87470cc6a40119bb2ad1a640000
Shenandoah Stubbs <shenandoahstubbs@mail.weber.edu>
2019-08-05 19:12:06 -0600

    checkTable() added to playerMove

--- src/main/java/game/GameLogic.java
+++ src/main/java/game/GameLogic.java
@@ -190,27 +190,35 @@ public class GameLogic {
                     if (playStack == 0 && table0.get(0).playable) {
                         table0.add(0, pa1);
                         playerOne.remove(p1);
+                        checkTable();
                     } else if (playStack == 1 && table1.get(0).playable) {
                         table1.add(0, pa1);
                         playerOne.remove(p1);
+                        checkTable();
                     } else if (playStack == 2 && table2.get(0).playable) {
                         table2.add(0, pa1);
                         playerOne.remove(p1);
+                        checkTable();
                     } else if (playStack == 3 && table3.get(0).playable) {
                         table3.add(0, pa1);
                         playerOne.remove(p1);
+                        checkTable();
                     } else if (playStack == 4 && table4.get(0).playable) {
                         table4.add(0, pa1);
                         playerOne.remove(p1);
+                        checkTable();
                     } else if (playStack == 5 && table5.get(0).playable) {
                         table5.add(0, pa1);
                         playerOne.remove(p1);
+                        checkTable();
                     } else if (playStack == 6 && table6.get(0).playable) {
                         table6.add(0, pa1);
                         playerOne.remove(p1);
+                        checkTable();
                     } else if (playStack == 7 && table7.get(0).playable) {
                         table7.add(0, pa1);
                         playerOne.remove(p1);
+                        checkTable();
                     }
 
 
@@ -221,27 +229,35 @@ public class GameLogic {
                     if (playStack == 0 && table0.get(0).playable) {
                         table0.add(0, pa2);
                         playerTwo.remove(p2);
+                        checkTable();
                     } else if (playStack == 1 && table1.get(0).playable) {
                         table1.add(0, pa2);
                         playerTwo.remove(p2);
+                        checkTable();
                     } else if (playStack == 2 && table2.get(0).playable) {
                         table2.add(0, pa2);
                         playerTwo.remove(p2);
+                        checkTable();
                     } else if (playStack == 3 && table3.get(0).playable) {
                         table3.add(0, pa2);
                         playerTwo.remove(p2);
+                        checkTable();
                     } else if (playStack == 4 && table4.get(0).playable) {
                         table4.add(0, pa2);
                         playerTwo.remove(p2);
+                        checkTable();
                     } else if (playStack == 5 && table5.get(0).playable) {
                         table5.add(0, pa2);
                         playerTwo.remove(p2);
+                        checkTable();
                     } else if (playStack == 6 && table6.get(0).playable) {
                         table6.add(0, pa2);
                         playerTwo.remove(p2);
+                        checkTable();
                     } else if (playStack == 7 && table7.get(0).playable) {
                         table7.add(0, pa2);
                         playerTwo.remove(p2);
+                        checkTable();
                     }
 
                 }
